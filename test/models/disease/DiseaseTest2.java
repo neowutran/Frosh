fc40f0549f7d1bf0a19263bf4b2d6c0e8193511c
neowutran <yukichi@sephirothgeek.ch>
2013-11-15 21:27:32 +0100

    ajout test

---
+++ test/models/disease/DiseaseTest2.java
@@ -0,0 +1,78 @@
+
+package models.disease;
+
+import models.lifeform.Human;
+import models.states.Sick;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import demonstrateur.Frosh;
+
+public class DiseaseTest2 {
+
+    @Test
+    public void nextDay_A$( ) throws Exception {
+        new Frosh( );
+
+        Human hum = new Human( 2, 2 );
+        hum.setStates( new Sick( hum ) );
+
+        H1N1 h = new H1N1( hum );
+        h.setDayBeforeNextState( 3 );
+        hum.setDisease( h );
+
+        Assert.assertEquals( 3, h.getIncubationTime( ) );
+        Assert.assertEquals( 100, h.getMortality( ) );
+        Assert.assertEquals( 4, h.getRecoveryTime( ) );
+        Assert.assertEquals( 10, h.getContagiousTime( ) );
+
+        // On regarde le temps avant le prochain état
+        Assert.assertEquals( 3, ( int ) h.getDayBeforeNextState( ) );
+
+        // On passe au jour suivant
+        h.nextDay( );
+
+        // On vérifie que le temps avant le prochain état s'est décrémentée
+        Assert.assertEquals( 2, ( int ) h.getDayBeforeNextState( ) );
+
+        // On vérifie que les autres propriétés n'ont pas bougées
+        Assert.assertEquals( 3, h.getIncubationTime( ) );
+        Assert.assertEquals( 100, h.getMortality( ) );
+        Assert.assertEquals( 4, h.getRecoveryTime( ) );
+        Assert.assertEquals( 10, h.getContagiousTime( ) );
+
+        // --------------------------
+
+        Human hum2 = new Human( 2, 2 );
+        hum2.setStates( new Sick( hum2 ) );
+
+        H5N1 h5 = new H5N1( hum2 );
+        h5.setDayBeforeNextState( 5 );
+        hum2.setDisease( h );
+
+        Assert.assertEquals( 3, h5.getIncubationTime( ) );
+        Assert.assertEquals( 100, h5.getMortality( ) );
+        Assert.assertEquals( 4, h5.getRecoveryTime( ) );
+        Assert.assertEquals( 10, h5.getContagiousTime( ) );
+
+        Assert.assertEquals( 5, ( int ) h5.getDayBeforeNextState( ) );
+        h5.nextDay( );
+        h5.nextDay( );
+        Assert.assertEquals( 3, ( int ) h5.getDayBeforeNextState( ) );
+
+        h5.nextDay( );
+        h5.nextDay( );
+        h5.nextDay( );
+        Assert.assertEquals( 0, ( int ) h5.getDayBeforeNextState( ) );
+
+        Assert.assertEquals( 3, h5.getIncubationTime( ) );
+        Assert.assertEquals( 100, h5.getMortality( ) );
+        Assert.assertEquals( 4, h5.getRecoveryTime( ) );
+        Assert.assertEquals( 10, h5.getContagiousTime( ) );
+
+        // --------------------------
+
+    }
+
+}
